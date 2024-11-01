eaa09c9102e1fddf5505af23d766004048bcd8d1
MaruDei <160330423+MaruDei@users.noreply.github.com>
2024-11-01 11:48:24 +0300

    add template

---
+++ JS - 81 - ДЗ/main.js
@@ -0,0 +1,20 @@
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < numbers.length; i++) {
+//     numbers[i] % 2 === 0 && console.log(numbers[i]);
+// }
+
+const rainbow = [
+    "красный",
+    "оранжевый",
+    "желтый",
+    "зеленый",
+    "голубой",
+    "синий",
+    "фиолетовый"
+]
+for (let i = rainbow.length - 1; i >= 0; i--) {
+    console.log(rainbow[i]);
+}
+
+
