42b87bc3239f50d312f393d022d05a7d01b9af4a
MaruDei <160330423+MaruDei@users.noreply.github.com>
2024-11-01 11:47:52 +0300

    add template

---
+++ JS - 76 - ДЗ/main.js
@@ -0,0 +1,8 @@
+const firstName = "Maria"
+const lastName = "Frank"
+let age = 29
+const job = "Deputy Chief Accountant"
+const city = "Tver"
+
+console.log (firstName, lastName, age, "y.o.", job, city)
+
