581ae2de81cec64f9da0a6d4db86706c8328606a
MaruDei <160330423+MaruDei@users.noreply.github.com>
2024-11-01 11:48:42 +0300

    add template

---
+++ JS - 83 - ДЗ/main.js
@@ -0,0 +1,101 @@
+// const user = {
+//     name: "Alex",
+//     age: 23,
+//     isAdmin: false
+// }
+// console.log(user.age)
+
+// const users = {
+//     alex: {
+//         age: 23,
+//         isAdmin: false
+//     }, 
+//     john: {
+//         age: 23,
+//         isAdmin: true,
+//         sayHello(name) {
+//             console.log (`Hello ${name}`)
+//         }
+//     }
+// }
+
+// console.log(users.john)
+// users.john.sayHello (`Tom`)
+
+// const users = [
+//     {
+//         name: "alex",
+//         age: 23,
+//         isAdmin: false
+//     }, 
+//     {
+//         name: "john",
+//         age: 20,
+//         isAdmin: true
+//     }, 
+// ]
+
+// users.push({
+//     name: "ivan",
+//     age: 20,
+//     isAdmin: true
+// })
+
+// for (let i=0; i<users.length; i++) {
+//     console.log(users[i].age) 
+// }
+
+// const foo = "Hello world"
+// console.log(foo.toUpperCase())
+
+// ===================================================================Домашка===================================================================
+// const user = {
+//     name: "Maria",
+//     age: 29,
+//     city: "Tver"
+// }
+// console.log (user.name)
+
+// const user = {
+//     Maria: {
+//         sayHello (name) {
+//             console.log (`Hello ${name}`)
+//         }
+//     }
+// }
+// console.log(user.Maria)
+// user.Maria.sayHello (`Garry`)
+
+
+// const users = [
+//     {
+//     name: "Maria",
+//     age: 29, 
+//     isAdmin: false
+// },
+//     {
+//     name: "Kirill",
+//     age: 32, 
+//     isAdmin: false
+// },
+//     {
+//     name: "Alex",
+//     age: 20, 
+//     isAdmin: true
+// },
+//     {
+//     name: "Anna",
+//     age: 50, 
+//     isAdmin: false
+// },
+// ]
+
+// let simpleUSers = 0;
+
+// for (let a=0; a<users.length; a++) {
+//     if (!users [a].isAdmin) {
+//         simpleUSers++;
+//     }
+// }
+
+// console.log("Количество простых пользователей", simpleUSers)
