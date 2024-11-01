4157dc91ac1937fd6c40dce1c30953b6da2a8b66
MaruDei <160330423+MaruDei@users.noreply.github.com>
2024-11-01 11:48:04 +0300

    add template

---
+++ JS - 79 - ДЗ/main.js
@@ -0,0 +1,11 @@
+const Maria = 29
+
+// if (Maria >= "18") {
+//     console.log ("Maria is an adult")
+// } else {
+//     (Maria <= "18")
+//     console. log ("Maria is not an adult")
+// }
+
+Maria >= "18" ? console.log ("Maria is an adult") : console.log ("Maria is not an adult")
+
