fa70ef6c381852b5644badef56c74c7a95005a59
MaruDei <160330423+MaruDei@users.noreply.github.com>
2024-11-01 11:48:34 +0300

    add template

---
+++ JS - 82 - ДЗ/main.js
@@ -0,0 +1,96 @@
+// function sumNumber (firstNumber, secondNumber) {
+//     return firstNumber + secondNumber
+// }
+
+// const result = sumNumber(3,10)
+// console.log(result)
+// console.log (sumNumber (-2, 2))
+
+// const users = ["John", "Ann", "Alex", "Max"]
+
+// const ChekForCopyItem = (array, item) => {
+//     for (let i=0; i<array.length; i++ ) {
+//         if (array [i] === item ) {
+//             return "There is a copy of the $ {item} in array"
+//         }
+//     }
+
+//  return "There is no such item in array"
+// }
+
+//  console.log (ChekForCopyItem(users,123))
+
+// ====================================================================Домашка===============================================================================
+
+// function firstWord () {
+//     return "Hello"
+// } 
+
+// const result = firstWord()
+// console.log (result)
+
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+// function checkNumbers(arr) {
+//     for (let a = 0; a < arr.length; a++) {
+//         if (arr[a] > 10) {
+//             console.log(arr[a]);
+//         }
+//     }
+// }
+
+// checkNumbers(numbers);
+
+
+// Сложение 
+// let num1 = 2;
+// let num2 = 3;
+// let operation = 'plus';
+
+// let result;
+
+// if (operation === 'plus') {
+//     result = num1 + num2;
+// if (result !== undefined) {
+//     console.log(`Результат операции ${result}`);
+// }}
+
+// Вычитание
+// let num1 = 2;
+// let num2 = 3;
+// let operation = 'minus';
+
+// let result;
+
+// if (operation === 'minus') {
+//     result = num1 - num2;
+// if (result !== undefined) {
+//     console.log(`Результат операции ${result}`);
+// }}
+
+// // Деление
+// let num1 = 2;
+// let num2 = 3;
+// let operation = 'divide';
+
+// let result;
+
+// if (operation === 'divide') {
+//     result = num1 / num2;
+// if (result !== undefined) {
+//     console.log(`Результат операции ${result}`);
+// }}
+
+// Умножение
+// let num1 = 2;
+// let num2 = 3;
+// let operation = 'multiply';
+
+// let result;
+
+// if (operation === 'multiply') {
+//     result = num1 * num2;
+// if (result !== undefined) {
+//     console.log(`Результат операции ${result}`);
+// }}
